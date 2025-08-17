6a2823f9bd1cea16ee08dd991650abd2322b3879
LarissaSCordeiro <larisstanckecordeiro@gmail.com>
2025-08-17 18:40:32 -0300

    Dump novo e capa dos jogos
    - Jogos autorizados e suas capas  add no banco/dump.

--- ludus.sql
+++ ludus.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `ludus` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `ludus`;
--- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
 --
 -- Host: localhost    Database: ludus
 -- ------------------------------------------------------
--- Server version	8.3.0
+-- Server version	9.1.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -91,7 +89,7 @@ CREATE TABLE `genero` (
   `nome` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nome` (`nome`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -100,7 +98,7 @@ CREATE TABLE `genero` (
 
 LOCK TABLES `genero` WRITE;
 /*!40000 ALTER TABLE `genero` DISABLE KEYS */;
-INSERT INTO `genero` VALUES (3,'Ação'),(5,'Aventura'),(8,'Cooperativo'),(9,'Corrida'),(11,'Esporte'),(7,'Estratégia'),(2,'Plataforma'),(4,'Puzzle'),(6,'RPG'),(12,'Tático'),(1,'Terror'),(10,'Tiro');
+INSERT INTO `genero` VALUES (1,'ação'),(13,'arcade'),(2,'aventura'),(8,'furtivo'),(10,'metroidvania'),(5,'musical'),(3,'narrativa'),(6,'puzzle'),(7,'retro'),(11,'rpg'),(12,'simulação'),(9,'tático'),(4,'terror');
 /*!40000 ALTER TABLE `genero` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -123,7 +121,7 @@ CREATE TABLE `jogo` (
   PRIMARY KEY (`id`),
   KEY `fk_jogo_usuario` (`id_usuario`),
   CONSTRAINT `fk_jogo_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -132,7 +130,7 @@ CREATE TABLE `jogo` (
 
 LOCK TABLES `jogo` WRITE;
 /*!40000 ALTER TABLE `jogo` DISABLE KEYS */;
-INSERT INTO `jogo` VALUES (1,'Enigma do Medo','Estúdio BR Games','Lucas Andrade','Um jogo de terror psicológico ambientado no interior do Brasil.','img/jogos/enigma_med.jpg','2023-10-01',1),(2,'Sombras de Dandara','Indie Flame','Ana Beatriz','Plataforma 2D com movimentos inspirados na resistência quilombola.','img/jogos/default.png','2024-02-15',1),(3,'Adore','QUByte Interactive','Cadabra Games','Adore é um action RPG onde você invoca e controla criaturas em tempo real, com foco em estratégia e personalização do estilo de combate.','img/jogos/Adore.jpeg','2023-08-03',1),(4,'GROBO','Lucas Molina','Lucas Molina','Uma narrativa sutil sobre crescimento e superação, usando apenas formas e mecânicas simples.','img/jogos/grobo.png','2020-08-01',1),(5,'No Place for Bravery','Ysbryd Games','Glitch Factory','Uma história intensa sobre coragem, perda e redenção, ambientada em um mundo marcado pela guerra.','img/jogos/No Place for Bravery.jpg','2022-09-22',1),(6,'Unsighted','Studio Pixel Punk','Studio Pixel Punk','Em uma cidade tomada por robôs conscientes, cada segundo conta para impedir a escuridão.','img/jogos/Unsighted.avif','2021-09-30',1),(7,'Horizon Chase Turbo','Aquiris','Aquiris','Uma celebração acelerada aos clássicos dos anos 90, com curvas perfeitas e trilha empolgante.','img/jogos/Horizon Chase Turbo.webp','2018-05-15',1),(8,'ARIDA: Backland\'s Awakening','Aoca Game Lab','Aoca Game Lab','Explore a realidade do sertão e descubra a força de sobreviver em um lugar onde o tempo é mais severo que o clima.','img/jogos/ARIDA.jpg','2019-08-15',1),(9,'Kaze and the Wild Masks','PixelHive','PixelHive','Viaje por mundos vibrantes e enfrente desafios usando os poderes de antigas entidades mascaradas.','img/jogos/Kaze and the Wild Masks.jpeg','2021-03-26',1),(10,'Blazing Chrome','The Arcade Crew','JoyMasher','Parta em uma missão explosiva contra máquinas hostis em uma missão para recuperar o planeta.','img/jogos/Blazing Chrome.jpeg','2019-07-11',1),(11,'Elderand','Graffiti Games','Mantra / Sinergia Games','Em um mundo esquecido pelos deuses, encare monstros grotescos e paisagens arrepiantes em busca de glória.','img/jogos/Elderand.jpg','2023-02-16',1),(12,'Chroma Squad','Behold Studios','Behold Studios','Gerencie uma equipe de dublês que decide filmar sua própria série de ação colorida, enfrentando desafios dentro e fora das câmeras.','img/jogos/Chroma Squad.jpg','2015-04-30',1),(13,'Celeste','Matt Makes Games','Matt Thorson + Studio MiniBoss','Acompanhe a jornada de uma jovem que decide escalar uma montanha misteriosa — e seus próprios limites.','img/jogos/Celeste.png','2018-01-25',1),(14,'Out of Space','Behold Studios','Behold Studios','Uma mudança de casa vira um caos intergaláctico quando visitantes indesejados ocupam seu novo lar.','img/jogos/Out of Space.webp','2020-02-26',1),(15,'Dodgeball Academia','Pocket Trap','Pocket Trap','Entre em uma escola onde queimada é mais do que um esporte — é o centro de amizades, rivalidades e aventuras.','img/jogos/Dodgeball Academia.jpg','2021-08-05',1),(16,'Red Ronin','Wired Dreams Studio','Thiago Oliveira','Um combate frio e calculado acompanha a vingança de uma assassina que deixou de seguir ordens.','img/jogos/Red Ronin.jpg','2020-10-13',1),(17,'Tetragon','Cafundo Creative Studio','Cafundo Creative Studio','Mude a gravidade e molde o mundo ao seu redor enquanto persegue a verdade escondida entre dimensões.','img/jogos/Tetragon.jpg','2021-08-12',1),(18,'Spacelines from the Far Out','Coffeenauts','Coffeenauts','Transforme voos caóticos em experiências memoráveis enquanto administra sua empresa nas estrelas.','img/jogos/Spacelines from the Far Out.jpg','2022-06-07',1),(19,'Fobia: St. Dinfna Hotel','Maximum Games','Pulsatrix Studios','Você entra para investigar... mas logo percebe que o hotel esconde memórias e algo mais.','img/jogos/Fobia.png','2022-06-28',1),(20,'Hellclock','Wulkan Studios','Wulkan Studios','Um conflito ancestral conhecido como Guerra da Eternidade ecoa pelas noites silenciosas de uma cidade esquecida. Você acorda sem memória, com um relógio amaldiçoado preso ao pulso, e precisa atravessar batalhas fantasmagóricas entre dimensões para entender seu papel nesse ciclo sem fim.','img/jogos/Hellclock.webp','2022-10-18',1),(21,'O Jogo Fodão','Estúdio do Teste','Desenvolvedor de teste','Um jogo fodão pra cacete onde tu bla bla bla','img/jogos/jogo_689e42c859fe67.58108980.webp','2025-06-03',2);
+INSERT INTO `jogo` VALUES (1,'A Lenda do Herói','Dumativa','Dumativa','A Lenda do Herói é um jogo de plataforma 2D com visual retrô, onde o personagem atravessa fases cheias de inimigos e obstáculos para salvar a princesa, tudo acompanhado por uma trilha sonora dinâmica criada pelos Castro Brothers que canta cada movimento em tempo real.','img/jogos/lenda-do-heroi.webp','2020-04-30',1),(2,'Enigma do Medo','Dumativa','Dumativa','Enigma do Medo é um jogo de investigação e terror com visual em pixel art e cenários 3D, inspirado na obra Ordem Paranormal criada por Cellbit. A protagonista Mia parte em busca do pai desaparecido ao lado de seu cão Lupi, explorando uma região tomada por fenômenos sobrenaturais e resolvendo enigmas pelo caminho.','img/jogos/enigma-do-medo.webp','2024-09-28',1),(3,'Shieldmaiden','Dumativa','Dumativa','Shieldmaiden é um jogo de ação e plataforma 2D com visual pixel art e clima cyberpunk. Asta, a protagonista, embarca em uma jornada por uma cidade devastada, enfrentando máquinas e usando um escudo energético para superar desafios.','img/jogos/shieldmaiden.jpg','2020-03-12',1),(4,'Dandara','Long Hat House','Long Hat House','Metroidvania brasileiro com arte pixelada e mecânicas de gravidade únicas.','img/jogos/dandara.jpg','2022-02-06',1),(5,'9Kings','Sad Socket','Sad Socket','Construtor de reinos roguelike com batalhas épicas e mecânicas de cartas.','img/jogos/9-kings.webp','2025-05-23',1),(6,'Gods of Sand','Sad Socket','Achenar Studios','Gerencie gladiadores em combates táticos por turnos em uma arena brutal.','img/jogos/gods-of-sand.jpeg','2022-02-07',1),(7,'Ending Tau','Sad Socket','Extra Small, Sad Socket','Roguelite de ação inspirado no folclore sul-americano, com sistema de maldições e transformação corporal.','img/jogos/ending-tau.jpg','2026-01-01',1),(8,'Seraphs\'s Last Stand','Sad Socket','Sad Socket','Roguelike arcade onde você cria builds insanas para enfrentar ondas infinitas de inimigos.','img/jogos/seraphs-last-stand.jpeg','2022-03-23',1);
 /*!40000 ALTER TABLE `jogo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -159,7 +157,7 @@ CREATE TABLE `jogo_possui_genero` (
 
 LOCK TABLES `jogo_possui_genero` WRITE;
 /*!40000 ALTER TABLE `jogo_possui_genero` DISABLE KEYS */;
-INSERT INTO `jogo_possui_genero` VALUES (1,1),(19,1),(20,1),(2,2),(4,2),(9,2),(13,2),(2,3),(3,3),(5,3),(6,3),(9,3),(10,3),(11,3),(20,3),(4,4),(16,4),(17,4),(3,5),(6,5),(8,5),(17,5),(5,6),(11,6),(12,6),(15,6),(12,7),(14,7),(18,7),(14,8),(18,8),(7,9),(10,10),(15,11),(16,12);
+INSERT INTO `jogo_possui_genero` VALUES (3,1),(4,1),(7,1),(8,1),(1,2),(2,2),(3,2),(4,2),(7,2),(1,3),(2,3),(3,3),(7,3),(2,4),(1,5),(1,6),(2,6),(4,6),(1,7),(3,7),(4,7),(8,7),(2,8),(5,9),(6,9),(7,9),(4,10),(4,11),(5,11),(6,11),(7,11),(8,11),(5,12),(6,12),(8,13);
 /*!40000 ALTER TABLE `jogo_possui_genero` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -174,8 +172,10 @@ CREATE TABLE `jogo_possui_plataforma` (
   `id_jogo` int NOT NULL,
   `id_plataforma` int NOT NULL,
   PRIMARY KEY (`id_jogo`,`id_plataforma`),
-  KEY `fk_jogo_plataforma_plataforma` (`id_plataforma`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `fk_jogo_plataforma_plataforma` (`id_plataforma`),
+  CONSTRAINT `fk_jogo_plataforma_jogo` FOREIGN KEY (`id_jogo`) REFERENCES `jogo` (`id`),
+  CONSTRAINT `fk_jogo_plataforma_plataforma` FOREIGN KEY (`id_plataforma`) REFERENCES `plataforma` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -184,7 +184,7 @@ CREATE TABLE `jogo_possui_plataforma` (
 
 LOCK TABLES `jogo_possui_plataforma` WRITE;
 /*!40000 ALTER TABLE `jogo_possui_plataforma` DISABLE KEYS */;
-INSERT INTO `jogo_possui_plataforma` VALUES (1,2),(2,1),(3,2),(3,3),(3,4),(3,7),(4,5),(4,6),(5,2),(5,7),(6,2),(6,7),(6,8),(6,9),(7,1),(7,2),(7,3),(7,4),(7,7),(8,2),(8,5),(8,6),(9,2),(9,3),(9,4),(9,7),(10,2),(10,7),(10,8),(10,9),(11,2),(11,7),(11,8),(11,9),(12,1),(12,2),(12,5),(12,6),(12,7),(12,8),(12,9),(13,1),(13,7),(13,8),(13,9),(13,10),(13,11),(14,2),(14,7),(14,8),(14,9),(15,2),(15,3),(15,4),(15,7),(16,1),(16,2),(16,3),(16,4),(16,7),(17,2),(17,3),(17,4),(17,7),(18,2),(18,3),(19,2),(19,3),(19,4),(20,2);
+INSERT INTO `jogo_possui_plataforma` VALUES (1,2),(2,2),(3,2),(4,2),(5,2),(6,2),(7,2),(8,2),(4,3),(4,4),(4,5),(4,6),(1,7),(3,7),(4,7),(8,7),(3,8),(4,8),(4,9),(1,10),(4,10),(1,11),(4,11),(6,11),(7,11),(8,11),(1,12),(2,12),(3,12),(4,12),(5,12),(6,12),(7,12),(8,12);
 /*!40000 ALTER TABLE `jogo_possui_plataforma` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -200,7 +200,7 @@ CREATE TABLE `plataforma` (
   `nome` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nome` (`nome`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -209,7 +209,7 @@ CREATE TABLE `plataforma` (
 
 LOCK TABLES `plataforma` WRITE;
 /*!40000 ALTER TABLE `plataforma` DISABLE KEYS */;
-INSERT INTO `plataforma` VALUES (2,'PC'),(1,'Web'),(3,'Xbox'),(4,'Playstation'),(5,'Android'),(6,'iOS'),(7,'Nintendo Switch'),(8,'Xbox One'),(9,'Playstation 4'),(10,'macOS'),(11,'Linux');
+INSERT INTO `plataforma` VALUES (5,'Android'),(6,'iOS'),(11,'Linux'),(10,'macOS'),(7,'Nintendo Switch'),(2,'PC'),(4,'Playstation'),(9,'Playstation 4'),(1,'Web'),(12,'Windows'),(3,'Xbox'),(8,'Xbox One');
 /*!40000 ALTER TABLE `plataforma` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -231,7 +231,7 @@ CREATE TABLE `usuario` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `nome` (`nome`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -240,7 +240,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'dev_teste','dev@email.com','desenvolvedor','1234senha_hashada','img/usuarios/default.png','2025-06-13 11:07:37'),(2,'illpirox','pierrehenriquedemirandaneppel@gmail.com','administrador','$2y$10$1.7c3eQN2D91NYRo9Txx1.b0zdunRRPr44ZGr1bZQUEF7IUG.ymxq','img/usuarios/gatofofo.jpg','2025-07-21 19:22:52'),(3,'teste123','teste@gmail.com','desenvolvedor','$2y$10$MdRoKggRlnT8/aaqDFY6aemc10RAdo.FdFmCMHsf/wgg9MxDnZXA6','img/usuarios/default.png','2025-07-24 16:47:56');
+INSERT INTO `usuario` VALUES (1,'dev_teste','dev@email.com','desenvolvedor','1234senha_hashada','img/usuarios/default.png','2025-06-13 11:07:37');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -305,4 +305,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-08-14 17:21:16
+-- Dump completed on 2025-08-17 18:28:00
