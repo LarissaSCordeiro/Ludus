94231133dc8b803e8aa5a667cb22d04327574951
Jey <143282317+JheniferBender@users.noreply.github.com>
2025-10-30 13:23:32 -0300

    Correção sutil no header do admin dnv
    Tirei os icones e deixei somente os links em formato de texto mesmo.

--- Site/headers/header_admin.php
+++ Site/headers/header_admin.php
@@ -1,27 +1,3 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
-<style>
-    .icon-link {
-        display: inline-flex;
-        align-items: center;
-        justify-content: center;
-        width: 40px;
-        height: 40px;
-        border-radius: 50%;
-        background: #1f1f1f;
-        color: #fff;
-        margin-left: 10px;
-        transition: 0.3s ease;
-        font-size: 18px;
-        transition: all 0.3s ease-in-out;
-    }
-
-    .icon-link:hover {
-        background: #cd3dff;
-        color: #fff;
-        transform: scale(1.15) rotate(5deg);
-    }
-</style>
-
 <header>
     <figure class="logo">
         <a href="paginainicial.php"><img src="img/NewLudusLogo.png" alt="Logotipo"></a>
@@ -34,15 +10,8 @@
             <img src="<?= htmlspecialchars($_SESSION['foto_perfil'] ?? 'img/usuarios/default.png') ?>"
                 alt="Perfil do usuário" class="user-avatar">
         </a>
-        <a href="gerenciamento.php" class="icon-link" title="Gerenciamento">
-            <i class="fa-solid fa-gear"></i>
-        </a>
-
-        <a href="alteracao_exclusao_jogos.php" class="icon-link" title="Editar jogos">
-            <i class="fa-solid fa-pen-to-square"></i>
-        </a>
+        <a href="gerenciamento.php" title="Gerenciamento">Gerenciamento</a>
     </nav>
-
     <!-- Barra de pesquisa -->
     <div class="search-container">
         <form action="pesquisa.php" method="GET">
