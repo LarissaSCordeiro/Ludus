93b9c35f7878089c4cab2f4cfcddd22a8d51c210
Jey <143282317+JheniferBender@users.noreply.github.com>
2025-10-30 11:33:20 -0300

    Pequena mudança no header dos admins
    Trocados os links de navegação para outras páginas por ícones, reduzindo o espaço gasto no header.

--- Site/headers/header_admin.php
+++ Site/headers/header_admin.php
@@ -1,3 +1,27 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+<style>
+    .icon-link {
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        background: #1f1f1f;
+        color: #fff;
+        margin-left: 10px;
+        transition: 0.3s ease;
+        font-size: 18px;
+        transition: all 0.3s ease-in-out;
+    }
+
+    .icon-link:hover {
+        background: #cd3dff;
+        color: #fff;
+        transform: scale(1.15) rotate(5deg);
+    }
+</style>
+
 <header>
     <figure class="logo">
         <a href="paginainicial.php"><img src="img/NewLudusLogo.png" alt="Logotipo"></a>
@@ -7,11 +31,16 @@
     <nav id="nav" class="nav-links">
         <a href="filtragem.php">Games</a>
         <a href="perfil.php">
-            <img src="<?= htmlspecialchars($_SESSION['foto_perfil'] ?? 'img/usuarios/default.png') ?>" 
-                 alt="Perfil do usuário" 
-                 class="user-avatar">
+            <img src="<?= htmlspecialchars($_SESSION['foto_perfil'] ?? 'img/usuarios/default.png') ?>"
+                alt="Perfil do usuário" class="user-avatar">
         </a>
-		<a href="gerenciamento.php">Gerenciamento</a>
+        <a href="gerenciamento.php" class="icon-link" title="Gerenciamento">
+            <i class="fa-solid fa-gear"></i>
+        </a>
+
+        <a href="alteracao_exclusao_jogos.php" class="icon-link" title="Editar jogos">
+            <i class="fa-solid fa-pen-to-square"></i>
+        </a>
     </nav>
 
     <!-- Barra de pesquisa -->
